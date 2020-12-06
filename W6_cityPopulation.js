1e3fc3b0bb8131638379af5cfeb3e83c5880d8f7
Sage Chaehyun Jung <70481467+sagecj@users.noreply.github.com>
2020-12-06 14:14:08 -0500

    Update and rename W6_API.js to W6_cityPopulation.js

--- W6_cityPopulation.js
+++ W6_cityPopulation.js
@@ -7,49 +7,45 @@ The beating motion represents the business and noise of the city.
 */
 
 let myData;
-let ppl;
-let cityName;
+let cityName, ppl;
 let rCity = 0;
 
 function setup() {
 	createCanvas(450, 450);
 	httpGet("https://data.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop%40public&sort=population&facet=country", myCallback);
-	frameRate(30); //adjusts the speed
+	frameRate(30); //adjusted the speed
 }
 
 function myCallback(output) {
 	//console messages for checking the data set
 		// console.log(output);
 		// console.log(typeof output);
-
-	console.log(JSON.parse(output));
+		console.log(JSON.parse(output));
+	
+	//parsing API data
 	let dParsed = JSON.parse(output);
-
 	console.log(dParsed.records);
 	myData = dParsed.records;
 
 	//console messages to check the specific data value
 		// console.log(dParsed.records[0].fields.city);
 		// console.log(dParsed.records[0].fields.population);
-	//Keys; not in use
-		// let myKeys = Object.keys(dParsed.records);
-		// console.log(myKeys);
 }
 
 function draw() {
 	background(0);
 
-	//the center circle
+	//the main circles
 	ellipseMode(CENTER);
 	noFill();
 	stroke(215, 230, 250, 100);
 	strokeWeight(5);
-	ellipse(width/2, height / 2, random(50, ppl * 0.00002));
-	//added a smaller one for a enhance the effect
-	strokeWeight(2);
-	ellipse(width/2, height / 2, random(50, ppl * 0.00002 - 50));
+	ellipse(width / 2, height / 2, random(50, ppl * 0.00002));
+		//added a smaller one to enhance the effect
+		strokeWeight(2);
+		ellipse(width / 2, height / 2, random(50, ppl * 0.00002 - 50));
 	
-	//loading specific api data
+	//loading the specific api data
 	if (myData) {
 		ppl = myData[rCity].fields.population;
 		cityName = myData[rCity].fields.city;
